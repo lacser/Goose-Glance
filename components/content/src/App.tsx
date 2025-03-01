edaa8687b7046f8b9549a1ef76abb539897a929d
Yaron <yaronching@outlook.com>
2025-03-01 15:22:59 -0500

    added symbols component

--- components/content/src/App.tsx
+++ components/content/src/App.tsx
@@ -1,15 +1,21 @@
 import { useContextService } from "./utils/useContextService";
 import { DevContent } from "./components/DevContent";
 import { useAppSelector } from "./store/hooks";
+import Symbols from "./components/Symbols";
 
 function App() {
   useContextService();
   const devMode = useAppSelector((state) => state.settings.devMode);
 
   if (devMode) {
-    return (<DevContent />);
+    return <DevContent />;
   }
-  return <div>Content To Be Added</div>;
+  return (
+    <>
+      <h1>User Page Under Construction</h1>
+      <Symbols color="black">construction</Symbols>
+    </>
+  );
 }
 
 export default App;
